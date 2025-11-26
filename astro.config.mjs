ce97fa15a855e8d4b9a687a9fd584416f78f199c
Est3banR <eramirezmo100@gmail.com>
2025-11-25 21:08:22 -0600

    mjs updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
 
 export default defineConfig({
-  site: "https://Est3banR.io",
+  site: "https://github.com/Est3banR/Est3banR.io",
   integrations: [tailwind(), react()],
   vite: {
     resolve: {
